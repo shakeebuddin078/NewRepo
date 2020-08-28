f1149c0a0190177083a10e5d8c960382a735574e
Gopal Ramavath <gramavath@claritysoft.dev>
2020-08-28 17:39:23 +0530

    Complete

---
+++ MvcCrudwithCoreWebApp/Migrations/20200828053037_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcCrudwithCoreWebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    EmpCode = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Position = table.Column<string>(type: "varchar(50)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ MvcCrudwithCoreWebApp/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCrudwithCoreWebApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [DisplayName("Full Name")]
+        [Column(TypeName ="varchar(50)")]
+        [Required(ErrorMessage ="This field is required")]
+        public string FullName { get; set; }
+        [DisplayName("EmpCode")]
+        [Column(TypeName ="varchar(50)")]
+        public string EmpCode { get; set; }
+        [Column(TypeName ="varchar(50)")]
+        public string Position { get; set; }
+        [DisplayName("Office Location")]
+        [Column(TypeName ="varchar(50)")]
+        public string OfficeLocation { get; set; }
+    }
+}
